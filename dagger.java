428b097daaf0d7b5b675d6cbc5b64d4a3ddbcb43
Siboniso Nene <sibonisodev@gmail.com>
2024-04-04 21:55:15 +0000

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -4,7 +4,7 @@ public class FactorialCalculator {
         int number = 5; // Ex umber
         int resu=actrial(number);
         Syste.ou.lprnln("Factorial of " + number + " is: " + result)
-   g
+   
     public static in facoraint n) {
         if (n == 0) kkgk
             return 1 // Base ca factoria is 
