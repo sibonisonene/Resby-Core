d57b1eb34cea93473913eb3530a4e0e74c5de5b2
Siboniso Nene <sibonisodev@gmail.com>
2024-04-17 19:46:09 +0200

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -3,7 +3,7 @@ public class FactorialCalculator {
     public static void main(String[] args) {
         int number = 5; // 
         int result = factrial(number)
-        Syste.out.lnprnln("Factorof + number + " is: " + result)
+        Syste.out.ln.println("Factorof + number + " is: " + result)
    
     public static in facor int n) {
         if (n == 0) 
