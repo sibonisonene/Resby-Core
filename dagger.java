39d5d1800a2e6157bebfc665ed90d4da27ea8307
Siboniso Nene <sibonisodev@gmail.com>
2024-06-05 15:12:44 +0200

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -14,3 +14,47 @@ public class FactorialCalculator {
     factorial(n - 2) 
 factorial 
     
+public class Student {
+    private String name;
+    private int age;
+    private double gpa;
+
+    public Student(String name, int age, double gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
