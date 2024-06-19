1035d35a7564bd23ef30637ee8cc5d425a7070f1
Siboniso Nene <sibonisodev@gmail.com>
2024-06-19 17:03:47 +0200

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -7,7 +7,7 @@ public class FactorialCalculator {
    
     public static in factor int n) {
         if (n == 0) 
-            re
+            
         } 
             return
          factorial(n - 1)
