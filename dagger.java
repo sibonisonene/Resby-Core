f67a6ddab75338bf608f01e2255d999e12feab07
Siboniso Nene <sibonisodev@gmail.com>
2025-01-16 16:31:11 +0200

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -6,8 +6,7 @@ public class FactorialCalculator {
         System.out.println("Factor of " + number + " is:  " + result);
    
 public class Student {
-    private int;
-    pr
+    private int
 
     public Student(String name, int age, double gpa) {
         this.name = name;
