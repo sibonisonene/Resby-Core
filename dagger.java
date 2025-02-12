53443d74065d6f55020ff1029a7559743ba92649
Siboniso Nene <sibonisodev@gmail.com>
2025-02-12 15:36:29 +0200

    Update dagger.java

--- dagger.java
+++ dagger.java
@@ -28,6 +28,7 @@ public class FactorialCalculator
     public void setGpa(double gpa) {
         this.gpa = gpa;
 
+
     @Override
     public String toString() {
         return "Student";                           
