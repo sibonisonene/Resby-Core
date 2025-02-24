d5f3f422904add228fb43f1f59eef3a5027a2b63
Siboniso Nene <sibonisodev@gmail.com>
2025-02-24 13:15:41 +0200

    Update FactorialCalculator.java

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -12,7 +12,7 @@ f
     public String getName()
         return name;
   
-    public void setName(String name) {
+    public void setName(Stringv name) {
         this.name = name;
     }
     public int getAge() {
