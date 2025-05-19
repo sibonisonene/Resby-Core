52e99ef7f89060efab9b8d2f491c4891c658f7d9
Siboniso Nene <sibonisodev@gmail.com>
2025-05-19 19:47:44 +0200

    Update FactorialCalculator.java

--- FactorialCalculator.java
+++ FactorialCalculator.java
@@ -7,6 +7,7 @@ public class FactorialCalculator
     public void Student(String name, int age, double gpa) {
         this.name = name;
         this.age = age; 
+
         this.gpa = gpa; 
 
     public String.getName()
